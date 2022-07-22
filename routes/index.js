d20d0fce31036e3f17cae0d3e58cda6a2b9e9fcb
ianchan3 <ianchan3138@gmail.com>
2022-07-22 15:05:04 -0700

    Google OAuth and Logo Render

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const passport = require("passport");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Feel The Heat?' });
+});
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    // Optionally force the user to pick account every time
+    prompt: 'select_account'
+  }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+));
+
+// Logout route
+router.get('/logout', function(req, res) {
+  req.logout(function(err) {
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
